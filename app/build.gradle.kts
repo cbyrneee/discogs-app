ae1a6bd4fa01f6cfc166aa85e430daad728a2777
Conor <71222289+cbyrneee@users.noreply.github.com>
2022-09-07 22:26:33 +0100

    refactor(build): use libs.versions.toml for version catalogs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+@file:Suppress("UnstableApiUsage")
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -25,7 +27,7 @@ android {
     }
 
     buildTypes {
-        val release by getting {
+        getByName("release") {
             isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
@@ -36,8 +38,10 @@ android {
 
     kotlinOptions {
         jvmTarget = "1.8"
-        freeCompilerArgs += "-opt-in=kotlinx.serialization.ExperimentalSerializationApi"
-        freeCompilerArgs += "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api"
+        freeCompilerArgs = listOf(
+            "-opt-in=kotlinx.serialization.ExperimentalSerializationApi",
+            "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api"
+        )
     }
 
     buildFeatures {
@@ -45,7 +49,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.2.0"
+        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
     }
 
     packagingOptions {
@@ -56,41 +60,16 @@ android {
 }
 
 dependencies {
-    val composeVersion = "1.2.1"
+    implementation(libs.bundles.androidx)
+    implementation(libs.bundles.androidx.compose)
+    implementation(libs.bundles.compose)
+    implementation(libs.bundles.hilt)
+    implementation(libs.bundles.kotlinx.serialization)
+    implementation(libs.bundles.lifecycle)
+    implementation(libs.bundles.okhttp)
+    implementation(libs.bundles.retrofit)
 
-    implementation("androidx.core:core-ktx:1.8.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
-    implementation("androidx.activity:activity-compose:1.5.1")
-    implementation("androidx.compose.ui:ui:$composeVersion")
-    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
-    implementation("androidx.compose.material3:material3:1.0.0-beta01")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
-    debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:$composeVersion")
+    debugImplementation(libs.bundles.compose.debug)
 
-    // compose navigation
-    implementation("androidx.navigation:navigation-compose:2.5.1")
-
-    // lifecycle
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.5.1")
-
-    // kotlinx serialization
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
-
-    // hilt dependency injection
-    implementation("com.google.dagger:hilt-android:2.43.2")
-    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
-    kapt("com.google.dagger:hilt-compiler:2.43.2")
-
-    // retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.okhttp3:okhttp:4.10.0")
-
-    // retrofit utilities
-    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
+    kapt(libs.hilt.compiler)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+// NOTE: Keep this suppression until https://youtrack.jetbrains.com/issue/KTIJ-19369 is closed
+@file:Suppress("DSL_SCOPE_VIOLATION")
+
 buildscript {
     dependencies {
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.43.2")
@@ -5,8 +8,8 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "7.4.0-alpha10" apply false
-    id("com.android.library") version "7.4.0-alpha10" apply false
-    id("org.jetbrains.kotlin.android") version "1.7.0" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.7.10" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlinx.serialization) apply false
 }
