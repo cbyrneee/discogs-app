51b848883aa3011cca2853223e78f44678a3817b
Conor <71222289+cbyrneee@users.noreply.github.com>
2022-09-08 17:09:05 +0100

    chore(ui): redesign the login screen

--- feature/auth/src/main/kotlin/dev/cbyrne/discogs/feature/auth/view/LoginScreen.kt
+++ feature/auth/src/main/kotlin/dev/cbyrne/discogs/feature/auth/view/LoginScreen.kt
@@ -1,18 +1,21 @@
 package dev.cbyrne.discogs.feature.auth.view
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import dev.cbyrne.discogs.feature.auth.view.model.LoginScreenViewModel
+import dev.cbyrne.discogs.ui.layout.CenteredColumn
 import kotlinx.coroutines.launch
 
 @Composable
@@ -26,15 +29,26 @@ fun LoginScreen(viewModel: LoginScreenViewModel = hiltViewModel()) {
         }
     }
 
-    Column(
-        modifier = Modifier
-            .padding(16.dp)
-            .fillMaxWidth(),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
+    CenteredColumn(
+        modifier = Modifier.padding(16.dp),
+        verticalAlignment = Arrangement.spacedBy(16.dp, Alignment.CenterVertically)
     ) {
-        Text("This will soon be a login screen")
-        Button(onClick = authorize) {
-            Text("Login")
+        Text(
+            text = "Welcome to the Discogs App",
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.titleMedium
+        )
+
+        Text(
+            text = "This is a third party application for the Discogs website. Clicking the button below will open your browser and authorize with your Discogs account.",
+            textAlign = TextAlign.Center
+        )
+
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = authorize
+        ) {
+            Text("Login to Discogs")
         }
     }
 }
