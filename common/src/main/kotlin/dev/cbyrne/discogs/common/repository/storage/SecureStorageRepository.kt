c0640a1cd2cb1d43b4c98d748c2fc2519358b376
Conor <71222289+cbyrneee@users.noreply.github.com>
2022-09-08 20:46:31 +0100

    feat(common): add support for serializable types in secure storage

--- common/src/main/kotlin/dev/cbyrne/discogs/common/repository/storage/SecureStorageRepository.kt
+++ common/src/main/kotlin/dev/cbyrne/discogs/common/repository/storage/SecureStorageRepository.kt
@@ -1,7 +1,21 @@
 package dev.cbyrne.discogs.common.repository.storage
 
+import dev.cbyrne.discogs.common.util.json
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+
 interface SecureStorageRepository {
     fun set(key: String, value: String)
     fun get(key: String): String?
     fun remove(key: String)
+}
+
+inline fun <reified T> SecureStorageRepository.getObject(key: String): T? {
+    val string = get(key) ?: return null
+    return kotlin.runCatching { json.decodeFromString<T>(string) }.getOrNull()
+}
+
+inline fun <reified T> SecureStorageRepository.setObject(key: String, value: T) {
+    val json = json.encodeToString(value)
+    set(key, json)
 }
