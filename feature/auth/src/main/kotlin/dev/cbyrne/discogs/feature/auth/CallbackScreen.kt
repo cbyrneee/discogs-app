c7e71a660c82c158f27037a064ce60d57c74c31e
Conor <71222289+cbyrneee@users.noreply.github.com>
2022-09-08 15:09:37 +0100

    refactor(feature/auth): use `CenteredColumn` from `ui` module

--- feature/auth/src/main/kotlin/dev/cbyrne/discogs/feature/auth/CallbackScreen.kt
+++ feature/auth/src/main/kotlin/dev/cbyrne/discogs/feature/auth/CallbackScreen.kt
@@ -1,18 +1,15 @@
 package dev.cbyrne.discogs.feature.auth
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import dev.cbyrne.discogs.ui.layout.CenteredColumn
 
 @Composable
 fun CallbackScreen(navigateToLogin: () -> Unit, token: String?) {
@@ -23,13 +20,7 @@ fun CallbackScreen(navigateToLogin: () -> Unit, token: String?) {
         }
     }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
+    CenteredColumn(modifier = Modifier.padding(16.dp)) {
         CircularProgressIndicator()
 
         Text(
