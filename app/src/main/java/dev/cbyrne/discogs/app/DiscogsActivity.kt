394b7dc521f3988c3fff66193ff141c40de580f0
Conor <71222289+cbyrneee@users.noreply.github.com>
2022-09-08 17:56:05 +0100

    fix(app): disable default status bar color

--- app/src/main/java/dev/cbyrne/discogs/app/DiscogsActivity.kt
+++ app/src/main/java/dev/cbyrne/discogs/app/DiscogsActivity.kt
@@ -2,18 +2,21 @@ package dev.cbyrne.discogs.app
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 import dev.cbyrne.discogs.app.navigation.CustomNavigationBar
 import dev.cbyrne.discogs.app.navigation.NavigationHost
@@ -32,10 +35,19 @@ class DiscogsActivity : ComponentActivity() {
             DiscogsTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
                 ) {
                     val navController = rememberNavController()
                     navHostController = navController
+
+                    val systemUiController = rememberSystemUiController()
+                    SideEffect {
+                        window.setFlags(
+                            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+                        )
+
+                        systemUiController.setStatusBarColor(color = Color.Transparent)
+                    }
 
                     val backStackEntry by navController.currentBackStackEntryAsState()
                     val currentScreen = currentRouteFromBackStack(backStackEntry)
