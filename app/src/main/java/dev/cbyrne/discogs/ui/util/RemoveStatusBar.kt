6106c2938c514c1a53912e52f8ea46e7dfa4f189
Conor <71222289+cbyrneee@users.noreply.github.com>
2022-09-09 17:41:09 +0100

    fix(app): show status bar icons correctly on light mode

--- app/src/main/java/dev/cbyrne/discogs/ui/util/RemoveStatusBar.kt
+++ app/src/main/java/dev/cbyrne/discogs/ui/util/RemoveStatusBar.kt
@@ -2,6 +2,7 @@ package dev.cbyrne.discogs.ui.util
 
 import android.view.Window
 import android.view.WindowManager
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
@@ -10,12 +11,17 @@ import com.google.accompanist.systemuicontroller.rememberSystemUiController
 @Composable
 fun RemoveStatusBar(window: Window) {
     val systemUiController = rememberSystemUiController()
+    val isSystemInDarkTheme = isSystemInDarkTheme()
+
     SideEffect {
         window.setFlags(
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
         )
 
-        systemUiController.setStatusBarColor(color = Color.Transparent)
+        systemUiController.setStatusBarColor(
+            color = Color.Transparent,
+            darkIcons = !isSystemInDarkTheme
+        )
     }
 }
