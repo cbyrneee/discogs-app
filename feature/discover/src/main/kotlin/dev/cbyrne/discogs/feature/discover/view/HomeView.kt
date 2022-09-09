b83b7495f3d7f171f1a3c56f8a0d1dd1ca334273
Conor <71222289+cbyrneee@users.noreply.github.com>
2022-09-09 17:49:12 +0100

    feat(feature/discovery): use new DiscogsLayout in HomeView

--- feature/discover/src/main/kotlin/dev/cbyrne/discogs/feature/discover/view/HomeView.kt
+++ feature/discover/src/main/kotlin/dev/cbyrne/discogs/feature/discover/view/HomeView.kt
@@ -1,9 +1,16 @@
 package dev.cbyrne.discogs.feature.discover.view
 
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import dev.cbyrne.discogs.ui.layout.DiscogsLayout
 
 @Composable
 fun HomeView() {
-    Text(text = "Home")
+    DiscogsLayout {
+        Text(
+            style = MaterialTheme.typography.bodyLarge,
+            text = "This is the home page"
+        )
+    }
 }
