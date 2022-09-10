98e1a43192238db6a434e3b78734ce0a6dd17ef6
Conor <71222289+cbyrneee@users.noreply.github.com>
2022-09-10 12:52:51 +0100

    feat(feature/discover): add basic profile information to the home view

--- feature/discover/src/main/kotlin/dev/cbyrne/discogs/feature/discover/view/HomeView.kt
+++ feature/discover/src/main/kotlin/dev/cbyrne/discogs/feature/discover/view/HomeView.kt
@@ -1,11 +1,22 @@
 package dev.cbyrne.discogs.feature.discover.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.SubcomposeAsyncImage
 import dev.cbyrne.discogs.feature.discover.view.model.HomeViewModel
 import dev.cbyrne.discogs.feature.discover.view.model.HomeViewState
 import dev.cbyrne.discogs.ui.layout.CenteredColumn
@@ -26,10 +37,29 @@ fun HomeView(
             is HomeViewState.Loaded -> {
                 val information = state.information
 
-                Text(
-                    style = MaterialTheme.typography.bodyLarge,
-                    text = "Hello, ${information.name} (${information.username})"
-                )
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    SubcomposeAsyncImage(
+                        model = information.avatarUrl,
+                        loading = {
+                            CircularProgressIndicator()
+                        },
+                        contentDescription = information.name,
+                        modifier = Modifier
+                            .size(50.dp)
+                            .clip(RoundedCornerShape(2.5.dp))
+                    )
+
+                    Column {
+                        Text(text = information.name)
+                        Text(
+                            text = information.username,
+                            modifier = Modifier.alpha(0.75f)
+                        )
+                    }
+                }
             }
 
             is HomeViewState.Loading -> {
