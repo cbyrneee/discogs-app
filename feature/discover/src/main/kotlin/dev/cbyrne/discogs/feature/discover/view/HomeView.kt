db6f3e6bc6beb7db61178c206eb516b8ef6fff5b
Conor <71222289+cbyrneee@users.noreply.github.com>
2022-09-10 13:10:29 +0100

    feat(feature/discover, libs): implement pull to refresh on home view

--- feature/discover/src/main/kotlin/dev/cbyrne/discogs/feature/discover/view/HomeView.kt
+++ feature/discover/src/main/kotlin/dev/cbyrne/discogs/feature/discover/view/HomeView.kt
@@ -1,11 +1,18 @@
 package dev.cbyrne.discogs.feature.discover.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import dev.cbyrne.discogs.feature.discover.component.ProfileCard
 import dev.cbyrne.discogs.feature.discover.view.model.HomeViewModel
 import dev.cbyrne.discogs.feature.discover.view.model.HomeViewState
@@ -17,6 +24,7 @@ fun HomeView(
     viewModel: HomeViewModel = hiltViewModel()
 ) {
     val state = viewModel.state
+    val refreshingState = rememberSwipeRefreshState(isRefreshing = viewModel.isRefreshing)
 
     LaunchedEffect(viewModel) {
         viewModel.load()
@@ -25,7 +33,18 @@ fun HomeView(
     DiscogsLayout(title = "Home") {
         when (state) {
             is HomeViewState.Loaded -> {
-                ProfileCard(information = state.information)
+                SwipeRefresh(
+                    state = refreshingState,
+                    onRefresh = { viewModel.reload() }
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .verticalScroll(rememberScrollState())
+                    ) {
+                        ProfileCard(information = state.information)
+                    }
+                }
             }
 
             is HomeViewState.Loading -> {
--- feature/discover/src/main/kotlin/dev/cbyrne/discogs/feature/discover/view/model/HomeViewModel.kt
+++ feature/discover/src/main/kotlin/dev/cbyrne/discogs/feature/discover/view/model/HomeViewModel.kt
@@ -4,9 +4,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.cbyrne.discogs.api.model.user.UserInformationModel
 import dev.cbyrne.discogs.common.repository.user.UserRepository
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 sealed class HomeViewState {
@@ -20,6 +22,7 @@ class HomeViewModel @Inject constructor(
     private val userRepository: UserRepository,
 ) : ViewModel() {
     var state by mutableStateOf<HomeViewState>(HomeViewState.Loading)
+    var isRefreshing by mutableStateOf(false)
 
     suspend fun load() {
         if (state is HomeViewState.Loaded) {
@@ -27,10 +30,18 @@ class HomeViewModel @Inject constructor(
         }
 
         state = HomeViewState.Loading
-        reload()
+        fetchData()
     }
 
-    suspend fun reload() {
+    fun reload() {
+        viewModelScope.launch {
+            isRefreshing = true
+            fetchData()
+            isRefreshing = false
+        }
+    }
+
+    private suspend fun fetchData() {
         val result = userRepository.current()
         state = result.fold(
             onSuccess = { HomeViewState.Loaded(it) },
